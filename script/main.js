9206897c80100bfe58881b970d20a45e73210d5d
princessubandoma <princessubandoma396@gmail.com>
2024-07-25 11:05:05 +0200

    script/main.js

---
+++ script/main.js
@@ -0,0 +1,9 @@
+const accessKey = "RZEIOVfPhS7vMLkFdd2TSKGFBS4o9_FmcV1Nje3FSjw";
+
+const formPl = document.querySelector("form");
+const searchInputPl = document.getElementById("search-input");
+const searchResultsPl = document.querySelector(".search-results");
+const showMoreButtonPl = document.getElementById("show-more-button");
+
+let inputData = "";
+let page = 1;
